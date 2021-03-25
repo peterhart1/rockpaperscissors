cc1f3c12c5362557f6444fed528ff65e18b7543e
Peter Hart <peterhart765@gmail.com>
2021-03-25 06:38:23 +0000

    Updated JS to game complete

--- scripts.js
+++ scripts.js
@@ -13,23 +13,39 @@ function computerPlay() { // returns computers option
     return options[getRandomInt()];
 }
 
+function toTitleCase(str) { //converts input to title case
+    return str.replace(
+        /\w\S*/g,
+        function (txt) {
+            return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+        }
+    );
+}
+
 function playRound(playerSelection) { // gets player option, versus computeroption, returns result message
+    playerSelection = toTitleCase(playerSelection);
     computerSelection = computerPlay()
     if ((playerSelection) === computerSelection) {
         return "It's a draw";
     }
-    else if (playerSelection === "Rock" && computerSelection === "Scissors") {
-        return "You win! " + "Your " + playerSelection + " beats " +  computerSelection;
-    }
-    else if (playerSelection === "Scissors" && computerSelection === "Paper") {
-        return "You win! " + "Your " + playerSelection + " beats " +  computerSelection;
+    else if ((playerSelection === "Rock" && computerSelection === "Scissors") ||
+        (playerSelection === "Scissors" && computerSelection === "Paper")) {
+        return "You win! " + "Your " + playerSelection + " beats " + computerSelection;
     }
     else if (playerSelection === "Paper" && computerSelection === "Rock") {
-        return "You win! " + "Your " + playerSelection + " beats " +  computerSelection;
-    }else{
-        return "You lose! " + computerSelection + " beats your " + playerSelection ;
+        return "You win! " + "Your " + playerSelection + " beats " + computerSelection;
+    } else {
+        return "You lose! " + computerSelection + " beats your " + playerSelection;
     }
 }
+
+function game() {
+    for (let round = 0; round < 5; round++) {
+        let playerSelection = prompt("Three, two, one...");
+        console.log(playRound(playerSelection));
+    }
+}
+
     //prompt player input
     // str transform input, normalise so case insensitive (diff function?)
     // if playerSelection == computerOption, rtn "!"
